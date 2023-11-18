3a0e7b1162273d3c1c1555629faafc687d110757
Yunier Alvarez <yunieralvarez@gmail.com>
2023-11-18 10:04:13 -0500

    rename

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use iced::{widget::text, Element, Sandbox, Settings};
 
 fn main() -> Result<(), iced::Error> {
-    App::run(Settings::default())
+    Notes::run(Settings::default())
 }
 
-struct App;
+struct Notes;
 
 #[derive(Debug)]
 enum Message {}
 
-impl Sandbox for App {
+impl Sandbox for Notes {
     type Message = Message;
 
     fn new() -> Self {
