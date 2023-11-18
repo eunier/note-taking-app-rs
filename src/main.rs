18a691dc4e5f49d60a1f3473857b09ae3ebcefcd
Yunier Alvarez <yunieralvarez@gmail.com>
2023-11-18 09:56:05 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::text, Element, Sandbox, Settings};
+
+fn main() -> Result<(), iced::Error> {
+    App::run(Settings::default())
+}
+
+struct App;
+
+#[derive(Debug)]
+enum Message {}
+
+impl Sandbox for App {
+    type Message = Message;
+
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn title(&self) -> String {
+        todo!()
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        todo!()
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        text("Hello").into()
+    }
 }
