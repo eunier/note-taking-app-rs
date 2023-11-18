fe147a0542f44164171fa548ae89a0c141b33a7c
Yunier Alvarez <yunieralvarez@gmail.com>
2023-11-18 18:42:48 -0500

    wip

--- ui_cosmic/src/main.rs
+++ ui_cosmic/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use cosmic::{
+    iced::{Error, Sandbox, Settings},
+    iced_widget::text,
+};
+
+fn main() -> Result<(), Error> {
+    Notes::run(Settings::default())
+}
+
+struct Notes;
+
+#[derive(Debug)]
+enum Message {}
+
+impl Sandbox for Notes {
+    type Message = Message;
+
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn title(&self) -> String {
+        todo!()
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        todo!()
+    }
+
+    fn view(
+        &self,
+        id: cosmic::iced_core::window::Id,
+    ) -> cosmic::iced::Element<'_, Self::Message> {
+        text("Hello").into()
+    }
+
+    fn close_requested(
+        &self,
+        id: cosmic::iced_core::window::Id,
+    ) -> Self::Message {
+        todo!()
+    }
 }
