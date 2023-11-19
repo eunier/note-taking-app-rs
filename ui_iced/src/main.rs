b4109bb1a935812579fa020ba430d7d408953da2
Yunier Alvarez <yunieralvarez@gmail.com>
2023-11-18 19:24:43 -0500

    switch to Application

--- ui_iced/src/main.rs
+++ ui_iced/src/main.rs
@@ -1,31 +1,44 @@
-use iced::{widget::text, Element, Sandbox, Settings};
+use iced::{
+    executor,
+    widget::{container, row, text},
+    Application, Command, Element, Sandbox, Settings, Theme,
+};
 
-#[tokio::main]
-async fn main() -> Result<(), iced::Error> {
+fn main() -> Result<(), iced::Error> {
     Notes::run(Settings::default())
 }
 
 struct Notes;
 
 #[derive(Debug)]
-enum Message {}
+enum Message {
+    NewNote,
+}
 
-impl Sandbox for Notes {
+impl Application for Notes {
+    type Executor = executor::Default;
+    type Flags = ();
     type Message = Message;
+    type Theme = Theme;
 
-    fn new() -> Self {
-        Self
+    fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
+        (Self {}, Command::none())
     }
 
     fn title(&self) -> String {
         String::from("Notes")
     }
 
-    fn update(&mut self, _message: Self::Message) {
-        todo!()
+    fn update(&mut self, _message: Self::Message) -> Command<Message> {
+        Command::none()
     }
 
     fn view(&self) -> Element<'_, Self::Message> {
+        // let side_menu =
         text("Hello").into()
+        // let mut widgets = vec![[]];
+        // // widgets.push(text("Hellot"));
+        // let new_note_btn = iced::widget::button(content)
+        // container(row!(widgets)).into()
     }
 }
