635c1244c9d3af98af426916c92845263775fa85
Yunier Alvarez <yunieralvarez@gmail.com>
2023-11-19 19:01:35 -0500

    wip

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -1,79 +1,22 @@
 use once_cell::sync::Lazy;
-use serde::{Deserialize, Serialize};
-use std::env::current_dir;
-pub use surrealdb;
-use surrealdb::engine::local::{Db, SpeeDb};
-use surrealdb::sql::Thing;
-use surrealdb::{Result, Surreal};
+use surrealdb::engine::remote::ws::Client;
+use surrealdb::engine::remote::ws::Ws;
+use surrealdb::opt::auth::Root;
+use surrealdb::Surreal;
 
-static DB: Lazy<Surreal<Db>> = Lazy::new(Surreal::init);
+static DB: Lazy<Surreal<Client>> = Lazy::new(Surreal::init);
 
-#[derive(Debug, Serialize)]
-struct Name<'a> {
-    first: &'a str,
-    last: &'a str,
-}
-
-#[derive(Debug, Serialize)]
-struct Person<'a> {
-    title: &'a str,
-    name: Name<'a>,
-    marketing: bool,
-}
-
-#[derive(Debug, Serialize)]
-struct Responsibility {
-    marketing: bool,
-}
-
-#[derive(Debug, Deserialize)]
-struct Record {
-    #[allow(dead_code)]
-    id: Thing,
-}
-
-pub async fn connect() -> Result<()> {
+async fn connect() -> Result<(), Box<dyn std::error::Error>> {
     let binding = current_dir()
         .unwrap()
         .join("db/database/note_taking_add_dev");
 
     let path = binding.to_str().unwrap();
-    let _ = DB.connect::<SpeeDb>(path);
+    // let _ = DB.connect::<SpeedDb>(path);
+    DB.se
     DB.use_ns("test").use_db("test").await?;
 
-    DB = Surreal::new::<SpeeDb>(path).await;
-
-    // Create a new person with a random id
-    let created: Vec<Record> = DB
-        .create("person")
-        .content(Person {
-            title: "Founder & CEO",
-            name: Name {
-                first: "Tobie",
-                last: "Morgan Hitchcock",
-            },
-            marketing: true,
-        })
-        .await?;
-    dbg!(created);
-
-    // Update a person record with a specific id
-    let updated: Option<Record> = DB
-        .update(("person", "jaime"))
-        .merge(Responsibility { marketing: true })
-        .await?;
-    dbg!(updated);
-
-    // Select all people records
-    let people: Vec<Record> = DB.select("person").await?;
-    dbg!(people);
-
-    // Perform a custom advanced query
-    let groups = DB
-        .query("SELECT marketing, count() FROM type::table($table) GROUP BY marketing")
-        .bind(("table", "person"))
-        .await?;
-    dbg!(groups);
+    DB.use_ns("namespace").use_db("database").await?;
 
     Ok(())
 }
@@ -104,4 +47,84 @@ pub async fn connect() -> Result<()> {
 //         let db = Surreal::new::<SpeeDb>(address).await?;
 //         Ok(Self { db })
 //     }
+// }
+
+// use once_cell::sync::Lazy;
+// use serde::{Deserialize, Serialize};
+// use std::env::current_dir;
+// pub use surrealdb;
+// use surrealdb::engine::local::{Db, SpeeDb};
+// use surrealdb::sql::Thing;
+// use surrealdb::{Result, Surreal};
+
+// static DB: Lazy<Surreal<Db>> = Lazy::new(Surreal::init);
+
+// #[derive(Debug, Serialize)]
+// struct Name<'a> {
+//     first: &'a str,
+//     last: &'a str,
+// }
+
+// #[derive(Debug, Serialize)]
+// struct Person<'a> {
+//     title: &'a str,
+//     name: Name<'a>,
+//     marketing: bool,
+// }
+
+// #[derive(Debug, Serialize)]
+// struct Responsibility {
+//     marketing: bool,
+// }
+
+// #[derive(Debug, Deserialize)]
+// struct Record {
+//     #[allow(dead_code)]
+//     id: Thing,
+// }
+
+// pub async fn connect() -> Result<()> {
+//     let binding = current_dir()
+//         .unwrap()
+//         .join("db/database/note_taking_add_dev");
+
+//     let path = binding.to_str().unwrap();
+//     let _ = DB.connect::<SpeeDb>(path);
+//     DB.use_ns("test").use_db("test").await?;
+
+//     DB = Surreal::new::<SpeeDb>(path).await;
+
+//     // Create a new person with a random id
+//     let created: Vec<Record> = DB
+//         .create("person")
+//         .content(Person {
+//             title: "Founder & CEO",
+//             name: Name {
+//                 first: "Tobie",
+//                 last: "Morgan Hitchcock",
+//             },
+//             marketing: true,
+//         })
+//         .await?;
+//     dbg!(created);
+
+//     // Update a person record with a specific id
+//     let updated: Option<Record> = DB
+//         .update(("person", "jaime"))
+//         .merge(Responsibility { marketing: true })
+//         .await?;
+//     dbg!(updated);
+
+//     // Select all people records
+//     let people: Vec<Record> = DB.select("person").await?;
+//     dbg!(people);
+
+//     // Perform a custom advanced query
+//     let groups = DB
+//         .query("SELECT marketing, count() FROM type::table($table) GROUP BY marketing")
+//         .bind(("table", "person"))
+//         .await?;
+//     dbg!(groups);
+
+//     Ok(())
 // }
