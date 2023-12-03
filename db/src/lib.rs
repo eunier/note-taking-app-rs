0a14785aa873d7fbf9cb32919770cbcd0d197c8f
Yunier Alvarez <yunieralvarez@gmail.com>
2023-12-03 12:57:01 -0500

    clean up

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -6,24 +6,6 @@ use surrealdb::engine::local::{Db, SpeeDb};
 use surrealdb::sql::Thing;
 use surrealdb::Surreal;
 
-#[derive(Debug, Serialize)]
-struct Name<'a> {
-    first: &'a str,
-    last: &'a str,
-}
-
-#[derive(Debug, Serialize)]
-struct Person<'a> {
-    title: &'a str,
-    name: Name<'a>,
-    marketing: bool,
-}
-
-#[derive(Debug, Serialize)]
-struct Responsibility {
-    marketing: bool,
-}
-
 #[derive(Debug, Deserialize, Clone)]
 pub struct Record {
     #[allow(dead_code)]
@@ -42,28 +24,8 @@ pub enum Error {
 
 // TODO remove all expect
 pub async fn create_note() -> Result<Vec<Record>, Error> {
-    // let created: Vec<Record> = db
-    // .create("person")
-    // .content(Person {
-    //     title: "Founder & CEO",
-    //     name: Name {
-    //         first: "Tobie",
-    //         last: "Morgan Hitchcock",
-    //     },
-    //     marketing: true,
-    // })
-    // .await?;
-
-    // return
-
-    // let a: Vec<Record> = db
-    //     .create("note")
-    //     .content(Note {
-    //         content: String::new(),
-    //     })
-    //     .await?;
-
     let db = connect().await?;
+
     let res: Vec<Record> = db
         .create("note")
         .content(Note {
@@ -73,32 +35,7 @@ pub async fn create_note() -> Result<Vec<Record>, Error> {
         .map_err(|err| Error::Message(err.to_string()))
         .expect("should create a note");
 
-    // let db_call: Vec<Record> = db
-    //     .create("note")
-    //     .content(Note {
-    //         content: String::new(),
-    //     })
-    //     .await;
-
-    // let b: Vec<Record> = match db
-    //     .create("note")
-    //     .content(Note {
-    //         content: String::new(),
-    //     }).await {
-
-    //     }
-
-    let notes: Vec<Record> = db
-        .select("note")
-        .await
-        .map_err(|err| Error::Message(err.to_string()))
-        .expect("should select all notes");
-    dbg!(&notes);
-    let notes_len = notes.len();
-    dbg!("{}", notes_len);
-
-    // db.select("note").await?
-    // Ok(a)
+    dbg!("created note", &res);
     Ok(res)
 }
 
@@ -120,157 +57,7 @@ pub async fn connect() -> Result<Surreal<Db>, Error> {
         .await
         .map_err(|err| Error::Message(err.to_string()))
         .expect("schould be able to select a namespace and database");
-    // db.use_ns("test").use_db("test").await?;
-
-    // Create a new person with a random id
-    // let created: Vec<Record> = db
-    //     .create("person")
-    //     .content(Person {
-    //         title: "Founder & CEO",
-    //         name: Name {
-    //             first: "Tobie",
-    //             last: "Morgan Hitchcock",
-    //         },
-    //         marketing: true,
-    //     })
-    //     .await
-    //     .map_err(|err| Error::Message(err.to_string()))
-    //     .expect("should be able to create a new person");
-
-    // dbg!(created);
-
-    // // Update a person record with a specific id
-    // let updated: Option<Record> = db
-    //     .update(("person", "jaime"))
-    //     .merge(Responsibility { marketing: true })
-    //     .await
-    //     .map_err(|err| Error::Message(err.to_string()))
-    //     .expect("should be able to update a person record");
-    // dbg!(updated);
-
-    // // Select all people records
-    // let people: Vec<Record> = db
-    //     .select("person")
-    //     .await
-    //     .map_err(|err| Error::Message(err.to_string()))
-    //     .expect("should be able to select all people records");
-    // dbg!(people);
-
-    // // Perform a custom advanced query
-    // let groups = db
-    //     .query("SELECT marketing, count() FROM type::table($table) GROUP BY marketing")
-    //     .bind(("table", "person"))
-    //     .await.map_err(|err| Error::Message(err.to_string())).expect("should be able to perform a custom query");
-    // dbg!(groups);
 
     dbg!("connected to database");
     Ok(db)
 }
-
-// use once_cell::sync::Lazy;
-// use serde::{Deserialize, Serialize};
-// use std::env::current_dir;
-// pub use surrealdb;
-// use surrealdb::engine::local::{Db, SpeeDb};
-// use surrealdb::sql::Thing;
-// use surrealdb::{Result, Surreal};
-
-// static DB: Lazy<Surreal<Db>> = Lazy::new(Surreal::init);
-
-// #[derive(Debug, Serialize)]
-// struct Name<'a> {
-//     first: &'a str,
-//     last: &'a str,
-// }
-
-// #[derive(Debug, Serialize)]
-// struct Person<'a> {
-//     title: &'a str,
-//     name: Name<'a>,
-//     marketing: bool,
-// }
-
-// #[derive(Debug, Serialize)]
-// struct Responsibility {
-//     marketing: bool,
-// }
-
-// #[derive(Debug, Deserialize)]
-// struct Record {
-//     #[allow(dead_code)]
-//     id: Thing,
-// }
-
-// pub async fn connect() -> Result<()> {
-//     let binding = current_dir()
-//         .unwrap()
-//         .join("db/database/note_taking_add_dev");
-
-//     let path = binding.to_str().unwrap();
-//     let _ = DB.connect::<SpeeDb>(path);
-//     DB.use_ns("test").use_db("test").await?;
-
-//     DB = Surreal::new::<SpeeDb>(path).await;
-
-//     // Create a new person with a random id
-//     let created: Vec<Record> = DB
-//         .create("person")
-//         .content(Person {
-//             title: "Founder & CEO",
-//             name: Name {
-//                 first: "Tobie",
-//                 last: "Morgan Hitchcock",
-//             },
-//             marketing: true,
-//         })
-//         .await?;
-//     dbg!(created);
-
-//     // Update a person record with a specific id
-//     let updated: Option<Record> = DB
-//         .update(("person", "jaime"))
-//         .merge(Responsibility { marketing: true })
-//         .await?;
-//     dbg!(updated);
-
-//     // Select all people records
-//     let people: Vec<Record> = DB.select("person").await?;
-//     dbg!(people);
-
-//     // Perform a custom advanced query
-//     let groups = DB
-//         .query("SELECT marketing, count() FROM type::table($table) GROUP BY marketing")
-//         .bind(("table", "person"))
-//         .await?;
-//     dbg!(groups);
-
-//     Ok(())
-// }
-
-// // #[derive(Debug, Serialize)]
-// // struct Note {
-// //     content: String,
-// // }
-
-// // #[derive(Clone, Debug, Deserialize)]
-// // struct Record {
-// //     #[allow(dead_code)]
-// //     id: Thing,
-// // }
-
-// // pub struct Database {
-// //     db: Surreal<surrealdb::engine::local::Db>,
-// // }
-
-// // impl Database {
-// //     pub async fn new() -> Result<Self> {
-// //         let a = DB.query("a");
-// //         println!("{:?}", a);
-// //         let path_buf = current_dir().unwrap();
-// //         let current_dir_display = path_buf.display();
-// //         let address =
-// //             format!("{current_dir_display}/db/database/note_taking_add_dev");
-// //         let db = Surreal::new::<SpeeDb>(address).await?;
-// //         Ok(Self { db })
-// //     }
-// // }
